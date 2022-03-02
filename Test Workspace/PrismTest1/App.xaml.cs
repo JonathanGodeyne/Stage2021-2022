c84b3473b1c66d05ef75b7a94840a7ba7fa0961a
JonathanGodeyne <11307408@student.pxl.be>
2022-03-02 11:52:41 +0100

    Big commit after first days, added notes for PXL assignments and added project for working with Prism

---
+++ Test Workspace/PrismTest1/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using PrismTest1.Views;
+using System;
+using System.Windows;
+
+namespace PrismTest1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<ShellWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
